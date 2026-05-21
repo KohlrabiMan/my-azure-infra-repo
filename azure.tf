4b28ba8be3f9524c76c80e0c3572ca577a5198d2
KohlrabiMan <42944861+KohlrabiMan@users.noreply.github.com>
2026-05-20 21:04:34 -0400

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -2,12 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80"
+      version = "=3.42.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+
+
+  use_cli = false
 }
